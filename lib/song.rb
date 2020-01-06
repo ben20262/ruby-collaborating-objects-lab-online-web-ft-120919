444e8f0ba398d1d927d1b1c00658de42d2c8c8e5
Benjamin Small <ben20262@aim.com>
2020-01-06 18:29:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,6 +14,7 @@ class Song
   def self.new_by_filename (file_name)
     clean = file_name.chomp(".mp3").split(" - ")
     inst = Song.new(clean[1])
+    puts inst
     puts Artist.all.select {|singer| singer.name == clean[0]}
   end
 
