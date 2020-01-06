8b64a994bf5f813e85d6ff2c5e1deb43d2030b16
Benjamin Small <ben20262@aim.com>
2020-01-06 18:37:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
     artist_name = clean[0]
     song_name = Song.new(song_name)
     Artist.find_or_create_by_name(artist_name)
-    artist_name.add_song(song_name)
+    puts Artist.all.select {|singer| singer.name == artist_name}
   end
 
   def artist_name= (artist_name)
